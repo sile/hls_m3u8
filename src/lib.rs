2690f2b1ebc73071400096705f86272d88c501ae
Takeru Ohta <phjgt308@gmail.com>
2018-02-14 12:00:41 +0900

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,33 @@
+//! [HLS] m3u8 parser/generator.
+//!
+//! [HLS]: https://tools.ietf.org/html/rfc8216
+//!
+//! # Examples
+//!
+//! TODO
+#![warn(missing_docs)]
 #[macro_use]
 extern crate trackable;
 
 pub use error::{Error, ErrorKind};
+pub use master_playlist::MasterPlaylist;
+pub use media_playlist::MediaPlaylist;
 
-pub mod attribute;
-pub mod master_playlist;
-pub mod media_playlist;
-pub mod media_segment;
-pub mod string;
+pub mod segment {
+    //! Media segment.
+    pub use super::media_segment::{MediaSegment, MediaSegmentBuilder};
+}
 pub mod tag;
 pub mod value;
 pub mod version;
 
+mod attribute;
 mod error;
-pub mod line;
+mod line;
+mod master_playlist;
+mod media_playlist;
+mod media_segment;
+mod string;
 
+/// This crate specific `Result` type.
 pub type Result<T> = std::result::Result<T, Error>;
