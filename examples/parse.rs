acbe7e73da0a08d089ad1a15a8c9f4c5d8cd14cb
Luro02 <24826124+Luro02@users.noreply.github.com>
2020-02-10 12:45:58 +0100

    disable examples

--- examples/parse.rs
+++ examples/parse.rs
@@ -1,29 +1,7 @@
-use clap::{App, Arg};
-use hls_m3u8::{MasterPlaylist, MediaPlaylist};
-use std::io::{self, Read};
+// use hls_m3u8::{MasterPlaylist, MediaPlaylist};
+// use std::fs::File;
+// use std::io::Read;
 
 fn main() {
-    let matches = App::new("parse")
-        .arg(
-            Arg::with_name("M3U8_TYPE")
-                .long("m3u8-type")
-                .takes_value(true)
-                .default_value("media")
-                .possible_values(&["media", "master"]),
-        )
-        .get_matches();
-    let mut m3u8 = String::new();
-    io::stdin().read_to_string(&mut m3u8).unwrap();
-
-    match matches.value_of("M3U8_TYPE").unwrap() {
-        "media" => {
-            let playlist: MediaPlaylist = m3u8.parse().unwrap();
-            println!("{}", playlist);
-        }
-        "master" => {
-            let playlist: MasterPlaylist = m3u8.parse().unwrap();
-            println!("{}", playlist);
-        }
-        _ => unreachable!(),
-    }
+    unimplemented!();
 }
